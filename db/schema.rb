e98a84f147e1c23913f1a99d6b8b3554380487fb
John Ratana <jratana1@gmail.com>
2023-03-21 13:07:09 -0400

    create_specs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_150313) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_164608) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,4 +21,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_150313) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "specs", force: :cascade do |t|
+    t.string "name"
+    t.datetime "date_completed"
+    t.bigint "customer_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_specs_on_customer_id"
+  end
+
+  add_foreign_key "specs", "customers"
 end
