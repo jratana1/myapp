bba7f42e8b5f732767f967a16896f99f0cd95cac
John Ratana <jratana1@gmail.com>
2023-03-24 11:39:30 -0400

    missing comma

--- db/migrate/20230324153447_create_boxes.rb
+++ db/migrate/20230324153447_create_boxes.rb
@@ -1,6 +1,6 @@
 class CreateBoxes < ActiveRecord::Migration[7.0]
   def change
-    create_table :boxes id: :uuid, default: 'gen_random_uuid()' do |t|
+    create_table :boxes, id: :uuid, default: 'gen_random_uuid()' do |t|
       t.string :barcode
       t.datetime :status_change
       t.integer :status, default: 0
